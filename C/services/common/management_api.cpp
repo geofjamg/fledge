ac980d286440473e7d390638dea23bb90f1bd839
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2024-03-27 17:54:54 +0100

    Wip
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- C/services/common/management_api.cpp
+++ C/services/common/management_api.cpp
@@ -193,7 +193,7 @@ string responsePayload;
  */
 void ManagementApi::configChange(shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request)
 {
-	std::cout << "WIN" << std::end;
+	std::cout << "WIN" << std::endl;
 ostringstream convert;
 string responsePayload;
 string payload;
